59cac68030215b7cf9eef9243caf82515da62204
David Bau <davidbau@mit.edu>
2019-03-21 09:11:00 -0400

    Add FSD measure.

---
+++ netdissect/frechet_distance.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+"""Calculates the Frechet Distance (FD) between two samples.
+
+Code apapted from https://github.com/bioinf-jku/TTUR to use PyTorch instead
+of Tensorflow
+
+Copyright 2018 Institute of Bioinformatics, JKU Linz
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import numpy as np
+import torch
+from scipy import linalg
+from scipy.misc import imread
+
+def sample_frechet_distance(sample1, sample2, eps=1e-6):
+    '''
+    Both samples should be numpy arrays.
+    Returns the Frechet distance.
+    '''
+    (mu1, sigma1), (mu2, sigma2) = [calculate_activation_statistics(s)
+            for s in [sample1, sample2]]
+    return calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=eps)
+
+def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
+    """Numpy implementation of the Frechet Distance.
+    The Frechet distance between two multivariate Gaussians X_1 ~ N(mu_1, C_1)
+    and X_2 ~ N(mu_2, C_2) is
+            d^2 = ||mu_1 - mu_2||^2 + Tr(C_1 + C_2 - 2*sqrt(C_1*C_2)).
+
+    Stable version by Dougal J. Sutherland.
+
+    Params:
+    -- mu1   : Numpy array containing the activations of a layer of the
+               inception net (like returned by the function 'get_predictions')
+               for generated samples.
+    -- mu2   : The sample mean over activations, precalculated on an
+               representative data set.
+    -- sigma1: The covariance matrix over activations for generated samples.
+    -- sigma2: The covariance matrix over activations, precalculated on an
+               representative data set.
+
+    Returns:
+    --   : The Frechet Distance.
+    """
+
+    mu1 = np.atleast_1d(mu1)
+    mu2 = np.atleast_1d(mu2)
+
+    sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)
+
+    assert mu1.shape == mu2.shape, \
+        'Training and test mean vectors have different lengths'
+    assert sigma1.shape == sigma2.shape, \
+        'Training and test covariances have different dimensions'
+
+    diff = mu1 - mu2
+
+    # Product might be almost singular
+    covmean, _ = linalg.sqrtm(sigma1.dot(sigma2), disp=False)
+    if not np.isfinite(covmean).all():
+        msg = ('fid calculation produces singular product; '
+               'adding %s to diagonal of cov estimates') % eps
+        print(msg)
+        offset = np.eye(sigma1.shape[0]) * eps
+        covmean = linalg.sqrtm((sigma1 + offset).dot(sigma2 + offset))
+
+    # Numerical error might give slight imaginary component
+    if np.iscomplexobj(covmean):
+        if not np.allclose(np.diagonal(covmean).imag, 0, atol=1e-3):
+            m = np.max(np.abs(covmean.imag))
+            raise ValueError('Imaginary component {}'.format(m))
+        covmean = covmean.real
+
+    tr_covmean = np.trace(covmean)
+
+    return (diff.dot(diff) + np.trace(sigma1) +
+            np.trace(sigma2) - 2 * tr_covmean)
+
+
+def calculate_activation_statistics(act):
+    """Calculation of the statistics used by the FID.
+    Params:
+    -- files       : List of image files paths
+    -- model       : Instance of inception model
+    -- batch_size  : The images numpy array is split into batches with
+                     batch size batch_size. A reasonable batch size
+                     depends on the hardware.
+    -- dims        : Dimensionality of features returned by Inception
+    -- cuda        : If set to True, use GPU
+    -- verbose     : If set to True and parameter out_step is given, the
+                     number of calculated batches is reported.
+    Returns:
+    -- mu    : The mean over samples of the activations of the pool_3 layer of
+               the inception model.
+    -- sigma : The covariance matrix of the activations of the pool_3 layer of
+               the inception model.
+    """
+    mu = np.mean(act, axis=0)
+    sigma = np.cov(act, rowvar=False)
+    return mu, sigma
---
+++ netdissect/fsd.py
@@ -0,0 +1,125 @@
+import torch, argparse, sys, os, numpy
+from netdissect.sampler import FixedRandomSubsetSampler, FixedSubsetSampler
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from netdissect.progress import default_progress, verbose_progress
+from netdissect import zdataset
+from netdissect import segmenter
+from netdissect import frechet_distance
+from netdissect import parallelfolder
+
+NUM_OBJECTS=336
+
+def main():
+    parser = argparse.ArgumentParser(description='Net dissect utility',
+            prog='python -m netdissect.fsd')
+    parser.add_argument('directory1')
+    parser.add_argument('directory2')
+    parser.add_argument('--size', type=int, default=10000)
+    if len(sys.argv) == 1:
+        parser.print_usage(sys.stderr)
+        sys.exit(1)
+    args = parser.parse_args()
+    if len(sys.argv) != 3:
+        parser.print_usage(sys.stderr)
+        sys.exit(1)
+    verbose_progress(True)
+    directory1, directory2 = args.directory1, args.directory2
+    t1, t2 = [cached_tally_directory(d, size=args.size)
+            for d in [directory1, directory2]]
+    print(frechet_distance.sample_frechet_distance(t1 * 100, t2 * 100))
+
+def cached_tally_directory(directory, size=10000):
+    filename = '%s_segtally_%d.npy' % (directory, size)
+    if os.path.isfile(filename):
+        return numpy.load(filename)
+    result = tally_directory(directory, size)
+    numpy.save(filename, result)
+    return result
+
+def tally_directory(directory, size=10000):
+    progress = default_progress()
+    dataset = parallelfolder.ParallelImageFolders(
+                [directory],
+                transform=transforms.Compose([
+                    transforms.Resize(256),
+                    transforms.CenterCrop(256),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                    ]))
+    loader = DataLoader(dataset,
+                        sampler=FixedRandomSubsetSampler(dataset, end=size),
+                        # sampler=FixedSubsetSampler(range(size)),
+                        batch_size=10, pin_memory=True)
+    upp = segmenter.UnifiedParsingSegmenter()
+    labelnames, catnames = upp.get_label_and_category_names()
+    result = numpy.zeros((size, NUM_OBJECTS), dtype=numpy.float)
+    batch_result = torch.zeros(loader.batch_size, NUM_OBJECTS,
+            dtype=torch.float).cuda()
+    with torch.no_grad():
+        batch_index = 0
+        for [batch] in progress(loader):
+            seg_result = upp.segment_batch(batch.cuda())
+            for i in range(len(batch)):
+                batch_result[i] = (
+                    seg_result[i,0].view(-1).bincount(
+                        minlength=NUM_OBJECTS).float()
+                    / (seg_result.shape[2] * seg_result.shape[3])
+                )
+            result[batch_index:batch_index+len(batch)] = (
+                    batch_result.cpu().numpy())
+            batch_index += len(batch)
+    return result
+
+def tally_dataset_objects(dataset, size=10000):
+    progress = default_progress()
+    loader = DataLoader(dataset,
+                        sampler=FixedRandomSubsetSampler(dataset, end=size),
+                        batch_size=10, pin_memory=True)
+    upp = segmenter.UnifiedParsingSegmenter()
+    labelnames, catnames = upp.get_label_and_category_names()
+    result = numpy.zeros((size, NUM_OBJECTS), dtype=numpy.float)
+    batch_result = torch.zeros(loader.batch_size, NUM_OBJECTS,
+            dtype=torch.float).cuda()
+    with torch.no_grad():
+        batch_index = 0
+        for [batch] in progress(loader):
+            seg_result = upp.segment_batch(batch.cuda())
+            for i in range(len(batch)):
+                batch_result[i] = (
+                    seg_result[i,0].view(-1).bincount(
+                        minlength=NUM_OBJECTS).float()
+                    / (seg_result.shape[2] * seg_result.shape[3])
+                )
+            result[batch_index:batch_index+len(batch)] = (
+                    batch_result.cpu().numpy())
+            batch_index += len(batch)
+    return result
+
+def tally_generated_objects(model, size=10000):
+    progress = default_progress()
+    zds = zdataset.z_dataset_for_model(model, size)
+    loader = DataLoader(zds, batch_size=10, pin_memory=True)
+    upp = segmenter.UnifiedParsingSegmenter()
+    labelnames, catnames = upp.get_label_and_category_names()
+    result = numpy.zeros((size, NUM_OBJECTS), dtype=numpy.float)
+    batch_result = torch.zeros(loader.batch_size, NUM_OBJECTS,
+            dtype=torch.float).cuda()
+    with torch.no_grad():
+        batch_index = 0
+        for [zbatch] in progress(loader):
+            img = model(zbatch.cuda())
+            seg_result = upp.segment_batch(img)
+            for i in range(len(zbatch)):
+                batch_result[i] = (
+                    seg_result[i,0].view(-1).bincount(
+                        minlength=NUM_OBJECTS).float()
+                    / (seg_result.shape[2] * seg_result.shape[3])
+                    )
+            result[batch_index:batch_index+len(zbatch)] = (
+                    batch_result.cpu().numpy())
+            batch_index += len(zbatch)
+    return result
+
+if __name__ == '__main__':
+    main()
