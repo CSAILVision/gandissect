286208fb963e013528f2bf6f331ed13131f6e31b
David Bau <davidbau@mit.edu>
2019-05-27 09:45:03 -0400

    Rename parameters, and handle dilations.

--- netdissect/upsample.py
+++ netdissect/upsample.py
@@ -1,8 +1,7 @@
 import torch
 
 def upsampler(data_shape, target_shape, input_shape=None, scale_offset=None,
-        modules=None, mode='bilinear', padding_mode='border',
-        dtype=torch.float, device=None):
+        convolutions=None, dtype=torch.float, device=None):
     '''
     Returns a function that will upsample a batch of torch data from the
     expected data_shape to the specified target_shape. Can use scale_offset
@@ -10,12 +9,13 @@ def upsampler(data_shape, target_shape, input_shape=None, scale_offset=None,
     maps feature pixels to input_shape pixels, and it is assumed that
     the target_shape is a uniform downsampling of input_shape.
     '''
-    if modules is not None:
+    if convolutions is not None:
         assert scale_offset is None
-        scale_offset = sequence_scale_offset(modules)
+        scale_offset = sequence_scale_offset(convolutions)
     grid = upsample_grid(data_shape, target_shape, input_shape, scale_offset,
             dtype, device)
-    def upsample_func(data):
+    # padding mode could be 'border'
+    def upsample_func(data, mode='bilinear', padding_mode='zeros'):
         nonlocal grid
         if data.device != grid.device:
             grid = grid.to(data.device)
@@ -42,31 +42,60 @@ def sequence_scale_offset(modulelist):
     However, AlexNet does not pad every layer, and after five stride-2
     reductions, the scale_offset is ((32, 31), (32, 31)).
     '''
-    return tuple(dilation_scale_offset(d) for d in dilations(modulelist))
+    return tuple(convconfig_scale_offset(d) for d in convconfigs(modulelist))
 
-def dilation_scale_offset(dilations):
-    '''Composes a lists of [(k, s, p)...] into a single total scale and offset
-    that returns to the input coordinates.
+def sequence_data_size(modulelist, input_size):
+    '''Returns (yscale, yoffset), (xscale, xoffset) given a list of modules.
+    To convert output coordinates back to input coordinates while preserving
+    centers of receptive fields, the affine transformation is:
+        inpx = outx * xscale + xoffset
+        inpy = outy * yscale + yoffset
+    In both coordinate systems, (0, 0) refers to the upper-left corner
+    of the first pixel, (0.5, 0.5) refers to the center of that pixel,
+    and (1, 1) refers to the lower-right corner of that same pixel.
+
+    Modern convnets tend to add padding to keep receptive fields centered
+    while scaling, which will result in zero offsets.  For example, after resnet
+    does five stride-2 reductions, the scale_offset is just ((32, 0), (32, 0)).
+    However, AlexNet does not pad every layer, and after five stride-2
+    reductions, the scale_offset is ((32, 31), (32, 31)).
     '''
-    if len(dilations) == 0:
+    return tuple(convconfig_data_size(d, s)
+            for d, s in zip(convconfigs(modulelist), input_size))
+
+def convconfig_scale_offset(convconfigs):
+    '''Composes a lists of [(k, d, s, p)...] into a single total scale and
+    offset that returns to the input coordinates.
+    '''
+    if len(convconfigs) == 0:
         return (1, 0)
-    scale, offset = dilation_scale_offset(dilations[1:])
-    kernel, stride, padding = dilations[0]
+    scale, offset = convconfig_scale_offset(convconfigs[1:])
+    kernel, dilation, stride, padding = convconfigs[0]
     scale *= stride
     offset *= stride
-    offset += (kernel - 1) / 2.0 - padding
+    offset += (kernel - 1) * dilation / 2.0 - padding
     return scale, offset
 
-def dilations(modulelist):
+def convconfig_data_size(convconfigs, data_size):
+    '''Applies a list of [(k, d, s, p)...] to the given input size to obtain
+    an output size.
+    '''
+    for kernel, dilation, stride, padding in convconfigs:
+        data_size = (1 + (data_size + 2 * padding
+            - dilation * (kernel - 1) - 1) // stride)
+    return data_size
+
+def convconfigs(modulelist):
     '''Converts a list of modules to a pair of lists of
-    [(kernel_size, stride, padding)...]: one for x, and another for y.'''
+    [(kernel_size, dilation, stride, padding)...]: one for x, and one for y.'''
     result = []
     for module in modulelist:
         settings = tuple(getattr(module, n, d)
-            for n, d in (('kernel_size', 1), ('stride', 1), ('padding', 0)))
+            for n, d in (('kernel_size', 1),
+                ('dilation', 1), ('stride', 1), ('padding', 0)))
         settings = tuple((s if isinstance(s, tuple) else (s, s))
             for s in settings)
-        if settings != ((1, 1), (1, 1), (0, 0)):
+        if settings != ((1, 1), (1, 1), (1, 1), (0, 0)):
             result.append(zip(*settings))
     return list(zip(*result))
 
