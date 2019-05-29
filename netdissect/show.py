8f2a7ddb42e6bec58046bff205a36acd9c281cbd
David Bau <davidbau@mit.edu>
2019-05-28 20:01:16 -0400

    Callable show that creates tables out of arrays of tuples.

--- netdissect/show.py
+++ netdissect/show.py
@@ -1,11 +1,22 @@
-import PIL, base64, io, IPython
+# show.py
+#
+# An abbreviated way to output simple HTML layout of text and images
+# into a python notebook.
+#
+# - show a PIL image to show an inline HTML <img>.
+# - show an array of items to vertically stack them, centered in a block.
+# - show an array of arrays to horizontally lay them out as inline blocks.
+# - show an array of tuples to create a table.
+
+import PIL, base64, io, IPython, types, sys
 import html as html_module
+from IPython.display import display
 
-def rows(obj):
-    return IPython.display.HTML('\n'.join(rows_tags(obj)))
+def blocks(obj, space='\n'):
+    return IPython.display.HTML(space.join(blocks_tags(obj)))
 
-def blocks(obj):
-    return IPython.display.HTML('\n'.join(blocks_tags(obj)))
+def rows(obj, space='\n'):
+    return IPython.display.HTML(space.join(rows_tags(obj)))
 
 def rows_tags(obj):
     if isinstance(obj, dict):
@@ -34,13 +45,45 @@ def blocks_tags(obj):
     elif isinstance(obj, IPython.display.HTML):
         results.append(obj.data)
     elif isinstance(obj, dict):
-        results.extend(blocks_tags([(v, k) for k, v in obj.items()]))
-    elif isinstance(obj, (list, tuple)) or hasattr(obj, '__iter__'):
-        results.append(
-            '<div style="display:inline-block; text-align:center">')
-        for line in obj:
-            results.extend(blocks_tags(line))
-        results.append('</div>')
+        results.extend(blocks_tags([(k, v) for k, v in obj.items()]))
+    elif hasattr(obj, '__iter__'):
+        blockstart, blockend, tstart, tend, rstart, rend, cstart, cend = [
+          '<div style="display:inline-block;text-align:center;line-height:1;' +
+             'vertical-align:top">',
+          '</div>',
+          '<table style="display:inline-table">',
+          '</table>',
+          '<tr style="padding:0">',
+          '</tr>',
+          '<td style="text-align:left; vertical-align:top; padding:1px">',
+          '</td>',
+          ]
+        needs_end = False
+        table_mode = False
+        for i, line in enumerate(obj):
+            if i == 0:
+                needs_end = True
+                if isinstance(line, tuple):
+                    table_mode = True
+                    results.append(tstart)
+                else:
+                    results.append(blockstart)
+            if table_mode:
+                results.append(rstart)
+                if not isinstance(line, str) and hasattr(line, '__iter__'):
+                    for cell in line:
+                        results.append(cstart)
+                        results.extend(blocks_tags(cell))
+                        results.append(cend)
+                else:
+                    results.append(cstart)
+                    results.extend(blocks_tags(line))
+                    results.append(cend)
+                results.append(rend)
+            else:
+                results.extend(blocks_tags(line))
+        if needs_end:
+            results.append(table_mode and tend or blockend)
     return results
 
 def pil_to_b64(img, format='png'):
@@ -51,4 +94,14 @@ def pil_to_b64(img, format='png'):
 def pil_to_html(img):
     return '<img src="data:image/png;base64,%s">' % (pil_to_b64(img))
     
+
+class CallableModule(types.ModuleType):
+    def __init__(self):
+        # or super().__init__(__name__) for Python 3
+        types.ModuleType.__init__(self, __name__)
+        self.__dict__.update(sys.modules[__name__].__dict__)
+    def __call__(self, x, *args, **kwargs):
+        return display(blocks(x, *args, **kwargs))
+
+sys.modules[__name__] = CallableModule()
 
